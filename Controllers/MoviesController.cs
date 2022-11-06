22004d0d21526984b63d49e1d6a2811bd2163d77
Merthynz <rerive@gmail.com>
2022-11-06 09:45:45 -0500

    Added Details Action to MoviesController

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -201,6 +201,39 @@ namespace MoviePro.Controllers
             return _context.Movie.Any(e => e.Id == id);
         }
 
+        public async Task<IActionResult> Details(int? id, bool local = false)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie movie = new();
+            if (local)
+            {
+                //Get the Movie data straight from the DB
+                movie = await _context.Movie.Include(m => m.Cast)
+                                            .Include(m => m.Crew)
+                                            .FirstOrDefaultAsync(m => m.Id == id);
+
+            }
+            else
+            {
+                //Get the movie data from the TMDB API
+                var movieDetail = await _tmdbMovieService.MovieDetailAsync((int)id);
+                movie = await _tmdbMappingService.MapMovieDetailAsync(movieDetail);
+
+            }
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["Local"] = local;
+            return View(movie);
+        }
+
         private async Task AddToMovieCollection(int movieId, string collectionName)
         {
             var collection = await _context.Collection.FirstOrDefaultAsync(c => c.Name == collectionName);
