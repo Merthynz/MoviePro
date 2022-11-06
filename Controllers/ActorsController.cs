2ca1b04f93af5b46f8e9b19f4a6235adafe6590f
Merthynz <rerive@gmail.com>
2022-11-06 16:14:12 -0500

    Added ActorsController

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviePro.Services.Interfaces;
+
+namespace MoviePro.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IRemoteMovieService _tmdbMovieServices;
+        private readonly IDataMappingService _mappingService;
+
+        public ActorsController(IRemoteMovieService tmdbMovieServices, IDataMappingService mappingService)
+        {
+            _tmdbMovieServices = tmdbMovieServices;
+            _mappingService = mappingService;
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var actor = await _tmdbMovieServices.ActorDetailAsync(id);
+            actor = _mappingService.MapActorDetail(actor);
+            return View(actor);
+        }
+    }
+}
