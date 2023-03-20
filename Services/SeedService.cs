b79db449bd00e3b741b38ea08d1cafa7627d9514
Merthynz <rerive@gmail.com>
2023-03-20 17:24:16 -0400

    Updated SeedService 4

--- Services/SeedService.cs
+++ Services/SeedService.cs
@@ -39,23 +39,26 @@ namespace MoviePro.Services
         {
             //if (_dbContext.Roles.Any())
             //    return;
-            var adminRole = _appSettings.MovieProSettings.DefaultCredentials.DCRole;
+            var adminRole = _appSettings.MovieProSettings.DefaultCredentials.DCRole ?? Environment.GetEnvironmentVariable("DCRole");
             await _roleManager.CreateAsync(new IdentityRole(adminRole));
         }
 
         private async Task SeedUsersAsync()
         {
-            if (_userManager.Users.Any())
-                return;
-            var credentials = _appSettings.MovieProSettings.DefaultCredentials;
+            //if (_userManager.Users.Any())
+            //    return;
+            var seedEmail = _appSettings.MovieProSettings.DefaultCredentials.DCEmail ?? Environment.GetEnvironmentVariable("DCEmail");
+            var seedPassword = _appSettings.MovieProSettings.DefaultCredentials.DCPassword ?? Environment.GetEnvironmentVariable("DCPassword");
+            var seedRole = _appSettings.MovieProSettings.DefaultCredentials.DCRole ?? Environment.GetEnvironmentVariable("DCRole");
+
             var newUser = new IdentityUser()
             {
-                Email = credentials.DCEmail,
-                UserName = credentials.DCEmail,
+                Email = seedEmail,
+                UserName = seedEmail,
                 EmailConfirmed = true
             };
-            await _userManager.CreateAsync(newUser, credentials.DCPassword);
-            await _userManager.AddToRoleAsync(newUser, credentials.DCRole);
+            await _userManager.CreateAsync(newUser, seedPassword);
+            await _userManager.AddToRoleAsync(newUser, seedRole);
         }
 
         private async Task SeedCollections()
